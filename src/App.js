27b6766dee09c00c9b4c7f1b020e2dc4c4c24a7b
rohit katbamna <rohitkatbamna@protonmail.com>
2022-04-16 10:31:49 +0530

    10:31 AM TOGGLE BUTTON COMPLETED

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
 	let eventvalue;
 	const counter = useSelector((state) => state.counter);
+	const toggleButton = useSelector((state) => state.toggleButton);
+	console.log(toggleButton);
 	const dispatch = useDispatch();
 	function increase() {
 		if (eventvalue === undefined) {
@@ -18,6 +20,9 @@ function App() {
 	function handleInput(event) {
 		eventvalue = Number(event.target.value);
 		console.log(eventvalue);
+	}
+	function toggleOn() {
+		dispatch({ type: "toggleIt" });
 	}
 	return (
 		<>
@@ -41,7 +46,19 @@ function App() {
 					Decrement
 				</button>
 				<input type="text" style={{ width: "50px" }} />
-				<h1 className="mt-3">{counter}</h1>
+				<h1
+					className="mt-3"
+					style={{ display: toggleButton ? "none" : "block" }}>
+					{counter}
+				</h1>
+			</div>
+			<div className="text-center">
+				<button
+					type="button"
+					className="btn btn-danger btn-lg mt-5"
+					onClick={toggleOn}>
+					Button Toggle
+				</button>
 			</div>
 		</>
 	);
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,16 +1,24 @@
 import { createStore } from "redux";
 
-const fristState = { counter: 0 };
+const fristState = { counter: 0, toggleButton: false };
 
 const counterReducer = (state = fristState, action) => {
 	if (action.type === "increment") {
 		return {
 			counter: state.counter + action.payload,
+			toggleButton: false,
 		};
 	}
 	if (action.type === "decrement") {
 		return {
 			counter: state.counter - action.payload,
+			toggleButton: false,
+		};
+	}
+	if (action.type === "toggleIt") {
+		return {
+			counter: state.counter,
+			toggleButton: !state.toggleButton,
 		};
 	}
 	return state;
